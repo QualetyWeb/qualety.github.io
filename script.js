51b9d0ec0be421e9d8bd61bbdd7950c4ab8f3b48
QualetyWeb <133808770+QualetyWeb@users.noreply.github.com>
2023-05-16 22:02:36 +0200

    updt

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const names = ["Bob", "Jeff", "Jonas", "Stephanie"];
+window.addEventListener("DOMContentLoaded", function() {
+    const names = ["Bob", "Jeff", "Jonas", "Stephanie"];
+    const namesList = document.getElementById("namesList");
 
-window.addEventListener("DOMContentLoaded", () => {
-  const namesList = document.getElementById("names-list");
-  names.forEach((name) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = name;
-    namesList.appendChild(listItem);
-  });
+    names.forEach(function(name) {
+        const listItem = document.createElement("li");
+        listItem.textContent = name;
+        namesList.appendChild(listItem);
+    });
 });
