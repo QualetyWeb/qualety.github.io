cce310881dfc61e07d6bc284c55203702bb71436
QualetyWeb <133808770+QualetyWeb@users.noreply.github.com>
2023-05-16 21:56:53 +0200

    update

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var tableBody = document.querySelector('#serverTable tbody');
-    
-    function fetchServerStatus() {
-      fetch('https://api.battlemetrics.com/servers?filter[game]=csgo&fields[server]=name,address,playerCount')
-        .then(function(response) {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Failed to fetch server status');
-          }
-        })
-        .then(function(data) {
-          populateServerTable(data.data);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    }
-    
-    function populateServerTable(serverData) {
-      tableBody.innerHTML = '';
-      
-      serverData.forEach(function(server) {
-        var row = document.createElement('tr');
-        
-        var nameCell = document.createElement('td');
-        nameCell.textContent = server.attributes.name;
-        
-        var addressCell = document.createElement('td');
-        addressCell.textContent = server.attributes.address;
-        
-        var playerCountCell = document.createElement('td');
-        playerCountCell.textContent = server.attributes.playerCount;
-        
-        row.appendChild(nameCell);
-        row.appendChild(addressCell);
-        row.appendChild(playerCountCell);
-        
-        tableBody.appendChild(row);
-      });
-    }
-    
-    fetchServerStatus();
+// Array to store the names
+var names = ['Bob', 'Jeff', 'Jonas', 'Stephanie'];
+
+// Function to add a name to the list
+function addName() {
+  var nameInput = document.getElementById('nameInput');
+  var name = nameInput.value.trim();
+
+  if (name !== '') {
+    names.push(name);
+    nameInput.value = '';
+    renderNameList();
+  }
+}
+
+// Function to render the name list
+function renderNameList() {
+  var nameList = document.getElementById('nameList');
+  nameList.innerHTML = '';
+
+  names.forEach(function(name) {
+    var li = document.createElement('li');
+    li.textContent = name;
+    nameList.appendChild(li);
   });
-  
+}
+
+// Initial rendering of the name list
+renderNameList();
