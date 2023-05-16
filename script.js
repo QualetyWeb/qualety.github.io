43d45c42db860c1fbc9b86f83c34166223022f10
QualetyWeb <133808770+QualetyWeb@users.noreply.github.com>
2023-05-16 22:00:09 +0200

    Fixes

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-// Array to store the names
-var names = ['Bob', 'Jeff', 'Jonas', 'Stephanie'];
+const names = ["Bob", "Jeff", "Jonas", "Stephanie"];
 
-// Function to add a name to the list
-function addName() {
-  var nameInput = document.getElementById('nameInput');
-  var name = nameInput.value.trim();
-
-  if (name !== '') {
-    names.push(name);
-    nameInput.value = '';
-    renderNameList();
-  }
-}
-
-// Function to render the name list
-function renderNameList() {
-  var nameList = document.getElementById('nameList');
-  nameList.innerHTML = '';
-
-  names.forEach(function(name) {
-    var li = document.createElement('li');
-    li.textContent = name;
-    nameList.appendChild(li);
+window.addEventListener("DOMContentLoaded", () => {
+  const namesList = document.getElementById("names-list");
+  names.forEach((name) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = name;
+    namesList.appendChild(listItem);
   });
-}
-
-// Initial rendering of the name list
-renderNameList();
+});
